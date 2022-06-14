04312ef04129f1ed20357d3a518ae9d2ce94cc6c
DevLop-EC <christlopbsc12@gmail.com>
2022-06-14 16:04:34 -0500

    Add seeder data for users

--- Ecotec-api/database/factories/UserFactory.php
+++ Ecotec-api/database/factories/UserFactory.php
@@ -20,6 +20,15 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'dni' => $this->faker->unique()->numberBetween(1000000000, 9999999999),
+            'gender' => $this->faker->randomElement(['masculino', 'femenino']),
+            'nationality' => $this->faker->country(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'photo' => $this->faker->imageUrl(300, 300, 'people'),
+            'birthday' => $this->faker->dateTimeBetween('-60 years', '-18 years'),
+            'role' => $this->faker->randomElement(['admin', 'student', 'teacher']),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
